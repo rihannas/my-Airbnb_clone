a23433e60b939ae43f681bd9d918ce51cbdf36c1
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-07-06 20:03:10 +0300

    added sqlalchemy for city class

--- models/city.py
+++ models/city.py
@@ -3,9 +3,13 @@
 '''city module'''
 
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     '''City class'''
-    state_id = ''  # stores state id (foreingkey)
-    name = ''
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'),
+                      nullable=False)  # stores state id (foreingkey)
+    name = Column(String(60), nullable=False)
